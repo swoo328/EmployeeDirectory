9ba0d5d8d1e0c10b95a4a2ec7545121611d3f202
Steven Woo <swoo9875@gmail.com>
2021-05-01 14:54:45 -0400

    creating a header in the components folder

--- employee/src/App.js
+++ employee/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/Header/Header';
+import Employee from './components/Employee/Employee';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Employee/>
     </div>
   );
 }
