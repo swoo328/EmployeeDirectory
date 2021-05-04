8a45de36e4c27d932f23175a92e9e431644d493c
Steven Woo <swoo9875@gmail.com>
2021-05-04 02:44:17 -0400

    adding buttons to the page

--- employee/src/components/EmployeeTable.js
+++ employee/src/components/EmployeeTable.js
@@ -5,73 +5,84 @@ import Header from '../Header/Header';
 import API from "../utils/API";
 
 class EmployeeTable extends Component {
-    state = {
-        search: "",
-        employees: []
-    }
-    // Search the random user API
-    componentDidMount() {
-        this.findUsers();
-    }
+  state = {
+    search: "",
+    employees: []
+  }
+  // Search the random user API
+  componentDidMount() {
+    this.findUsers();
+  }
 
-    findUsers  = query => {
-        API.search(query)
-          .then(res => {
-            console.log(res.data.employees);
-            this.setState({ results: res.data.employees})
-          })
-          .catch(err => console.log(err))
-    };
+  //load random users as employees from the randomuser API
 
-    handleInputChange = event => {
-        const name = event.target.name;
-        const value = event.target.value;
+  findUsers = () => {
+    API.findUsers()
+      .then((res) => {
         this.setState({
-            [name]: value
-        });
-    };
-
-    handleFormSubmit = event => {
-        event.preventDefault();
-        const searchVal = document.querySelector("name=search").value;
-        let filtered = [...this.state.employees]
-        filtered = filtered.filter(employee => employee.name.first === searchVal)
-        this.setState({
-            employees: filtered
-      
-          })
-    }
-
-
-    handleRemove = id => {
-        console.log(id);
-        this.setState({
-          employees: this.state.employees.filter(employee => employee.id.value !== id)
-        });
+          employees: res.data.results,
+          filterEmployees: res.data.results
+        })
       }
-    
-      handleSort = (key1, key2, asc) => {
-        // copy
-        let employeeSorted = [...this.state.employees];
-    
-        // sort 
-        employeeSorted.sort((a, b) => {
-          return a[key][key2] > b[key1][key2] ? asc * 1 : asc * -1;
-        });
-    
-        // set state
-        this.setState({ employees: employeeSorted });
-    
-      }
-        render(){
-            return (
-                <div className="search-div">
-                    <Header />
-                    <Search />
+      )
+      .catch((err) => console(err));
+  }
 
-                </div>
-            )
-        }
+  handleInputChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    const searchVal = document.querySelector("name=search").value;
+    let filtered = [...this.state.employees]
+    filtered = filtered.filter(employee => employee.name.first === searchVal)
+    this.setState({
+      employees: filtered
+
+    })
+  }
+
+
+  handleRemove = id => {
+    console.log(id);
+    this.setState({
+      employees: this.state.employees.filter(employee => employee.id.value !== id)
+    });
+  }
+
+  handleSort = (key1, key2, asc) => {
+    // copy
+    let employeeSorted = [...this.state.employees];
+
+    // sort 
+    employeeSorted.sort((a, b) => {
+      return a[key][key2] > b[key1][key2] ? asc * 1 : asc * -1;
+    });
+
+    // set state
+    this.setState({ employees: employeeSorted });
+
+  }
+  render() {
+    return (
+      <div className="search-div">
+        <Header />
+        <Search
+          search={this.state.search}
+          handleFormSubmit={this.handleFormSubmit}
+          handleInputChange={this.handleInputChange}
+        />
+        <h3>Sort By:</h3>
+        <button onClick={() => this.handleSort('name', 'first', 1)}>Sort By First Name</button>
+        <button onClick={() => this.handleSort('name', 'last', 1)}>Sort By Last Name</button>
+      </div>
+    )
+  }
 }
 
 export default EmployeeTable;
